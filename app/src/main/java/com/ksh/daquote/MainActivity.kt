2d20369d7acb5f95f5b4bb8c29e274203ca6634f
Kungchi <a01027756032@gmail.com>
2024-10-09 19:52:13 +0900

    인터넷 연결 안되어있으면 앱 종료되게

--- app/src/main/java/com/ksh/daquote/MainActivity.kt
+++ app/src/main/java/com/ksh/daquote/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ksh.daquote
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.ksh.daquote.utility.AppDatabase
@@ -17,9 +21,27 @@ class MainActivity : AppCompatActivity() {
             AppDatabase::class.java, "quotes-db"
         ).build()
 
-        //메인 페이지 액티비티 시작
-        val intent = Intent(this, MainPageActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
+        if (!isNetworkAvailable()) {
+            Toast.makeText(this, "인터넷 연결이 필요합니다.", Toast.LENGTH_SHORT).show()
+            // 앱 종료
+            finish() // 또는 System.exit(0)
+        } else {
+            //메인 페이지 액티비티 시작
+            val intent = Intent(this, MainPageActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
     }
 }
